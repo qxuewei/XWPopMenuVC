73a5e62b1cc4b1af3a7e0610fc7b8543807f071f
qxuewei <qxuewei@yeah.net>
2016-05-17 23:34:38 +0800

    初始化

---
+++ XWPopMenuVCDemo/XWPopMenuVCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XWPopMenuVCDemo
+//
+//  Created by 邱学伟 on 16/5/17.
+//  Copyright © 2016年 邱学伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XWPopMenuVCDemo/XWPopMenuVCDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XWPopMenuVCDemo
+//
+//  Created by 邱学伟 on 16/5/17.
+//  Copyright © 2016年 邱学伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

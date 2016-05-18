135edb3819d01b9e3ad67b174e955e7ab1642179
qxuewei <qxuewei@yeah.net>
2016-05-18 10:30:51 +0800

    演示效果图

---
+++ XWPopMenu/PublishMenuButton.h
@@ -0,0 +1,13 @@
+//
+//  PublishMenuButton.h
+//  XWPopMenu
+//
+//  Created by 邱学伟 on 16/4/22.
+//  Copyright © 2016年 邱学伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PublishMenuButton : UIButton
+
+@end
